f8ee33fb7f26b55d8e64e900127943d17627cfc0
Alain Meurant <alain.meurant@affinitic.be>
2014-10-16 09:21:38 +0200

    Activation envoi de mail

--- interclps/skin/browser/ManageInterClps.py
+++ interclps/skin/browser/ManageInterClps.py
@@ -185,8 +185,8 @@ class ManageInterClps(BrowserView):
         mailer.setRecipients("alain.meurant@affinitic.be, l.polome@province.luxembourg.be, s.verscheure@province.luxembourg.be, %s" % clpsEmailContact)
         #mailer.setRecipients("alain.meurant@affinitic.be")
         mail = message
-        print message
-        #mailer.sendAllMail(mail)
+        #print message
+        mailer.sendAllMail(mail)
 
     def sendMailWhenLoginByAuteur(self, sujet, message):
         """
@@ -197,8 +197,8 @@ class ManageInterClps(BrowserView):
         mailer.setSubject(sujet)
         mailer.setRecipients("alain.meurant@skynet.be")
         mail = message
-        print message
-        #mailer.sendAllMail(mail)
+        #print message
+        mailer.sendAllMail(mail)
 
     def sendMailForNewAuteurExperience(self):
         """
@@ -5152,7 +5152,7 @@ class ManageInterClps(BrowserView):
 
         if operation == "update":
             self.updateAuteur()
-            #self.deleteLoginAuteur(auteurLogin)
+            self.deleteLoginAuteur(auteurLogin)
             if auteurEtat == 'True':
                 self.insertLoginAuteur(auteurLogin,
                                        auteurPassword,
